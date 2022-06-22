a8867c26e1f0904c57010af3ce7b3edfbfb52cc5
resourge <jdaoliveiraprograming@hotmail.com>
2022-06-22 15:07:01 +0100

    refactor(app.tsx): remove code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useSearchParams } from 'src/lib/hooks/useSearchParams';
-
 function App() {
-	const [
-		{
-			url
-		},
-		setParams
-	] = useSearchParams(
-		({ url }) => {
-			window.history.replaceState(null, '', url.href)
-		},
-		{
-
-		},
-		{
-			hash: true
-		}
-	)
-	
 	return (
 		<div>
 			App
-			<button
-				onClick={() => {
-					setParams({
-						test: 1
-					})
-				}}
-			>
-				Set Params
-			</button>
 		</div>
 	)
 }
