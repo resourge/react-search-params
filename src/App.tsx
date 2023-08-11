4350a9602e4544ea498d8beb92f2dd788709846e
resourge <jdaoliveiraprograming@hotmail.com>
2023-08-11 12:20:55 +0100

    chore(app): remove console logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useUrl } from './lib'
-
 function App() {
-	const [url, action, previousUrl] = useUrl();
-
-	console.log('url', url); 
-	console.log('action', action); 
-	console.log('previousUrl', previousUrl)
-
 	return (
 		<div>
 			App
-			<button onClick={() => {
-				window.history.pushState(null, '', Math.random().toString())
-			}}
-			>
-				Test
-			</button>
 		</div>
 	)
 }
